930c071501245882c0994a058877212f6d43ae28
Mikhail Nikonov <mnikonov@sessionm.com>
2022-07-19 23:05:05 +0200

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
+import Script from 'next/script';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-M2NHXX2TCJ`} />
+      <Script strategy="lazyOnload" id="google-analytics">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-M2NHXX2TCJ', {
+              page_path: window.location.pathname,
+              });
+          `}
+        </Script>
+
+      <Component {...pageProps} />  
+    </>
+  )
 }
 
 export default MyApp
